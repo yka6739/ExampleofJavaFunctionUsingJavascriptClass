f6463941db41685c11eb9591c5bbb4d4ca9cca7d
YATENDRA AWANA <yaten96@live.com>
2021-07-17 19:51:43 +0530

    Add project files.

---
+++ ExampleofJavaFunctionUsingJavascriptClass/Client/AllData/JavaScriptclass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleofJavaFunctionUsingJavascriptClass.Client.AllData
+{
+    public static class JavaScriptclass
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            return await js.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
---
+++ ExampleofJavaFunctionUsingJavascriptClass/Client/Pages/Index.razor.cs
@@ -0,0 +1,35 @@
+﻿using ExampleofJavaFunctionUsingJavascriptClass.Client.AllData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleofJavaFunctionUsingJavascriptClass.Client.Pages
+{
+    public partial class Index
+    {
+        private List<Data> d;
+        protected override void OnInitialized()
+        {
+            d = new List<Data>()
+            {
+
+            new Data() { Id = 101, Name = "Aman", Address = "Noida" },
+                new Data() { Id = 102, Name = "manish", Address = "Gr Noida" },
+                new Data() { Id = 103, Name = "Ajay", Address = "Noida" },
+                new Data() { Id = 104, Name = "Vijay", Address = "Gr Noida" }
+            };
+        }
+        private async Task DeleteAsync(Data d1)
+        {
+
+            var confirmed = await js.Confirm($"Are You Sure Want to Delete{d1.Id}{d1.Name}{d1.Address}");
+            if (confirmed)
+            {
+                d.Remove(d1);
+            }
+        }
+        public bool check;
+    
+}
+}
